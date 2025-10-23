76596bacc35ada12882e0c90c0086c2df9befbe6
ketarubot <robin09v@naver.com>
2025-10-23 12:49:37 +0900

    define types: Book, Status, BookState

---
+++ src/types.ts
@@ -0,0 +1,13 @@
+export interface Book {
+  name: string;
+  author: string;
+}
+
+type Status = "다 읽음" | "읽는 중" | "관심 있음";
+export interface BookState {
+  book: Book;
+  curPage: number;
+  lastPage: number;
+  aim?: Date;
+  status: Status;
+}
