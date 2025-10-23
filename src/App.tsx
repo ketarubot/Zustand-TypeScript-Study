4d851a3e0b8b6169dbcfdda7ae31045bec538f7a
ketarubot <robin09v@naver.com>
2025-10-23 12:48:55 +0900

    add pages & connect Route

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import DashBoard from "./pages/DashBoard";
+import WholeBookList from "./pages/WholeBookList";
+
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<DashBoard />} />
+        <Route path="/list" element={<WholeBookList />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
